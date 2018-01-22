0380819fd0c537019f2a8d834aa943ab71ecc4dc
Dario Ghirardo <dario.ghirardo@gmail.com>
2018-01-22 01:07:10 +0100

    Add features

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const Twit = require('twit');
 const config = require('./config')
 const T = new Twit(config);
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
+app.use(express.static('public'));
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false}));
 
-app.use((req, res, next) => {
+
+app.use(
+    (req, res, next) => {
       T.get('account/verify_credentials', function(err, data, response) {
         req.account = data;
         next();
@@ -20,26 +25,44 @@ app.use((req, res, next) => {
       }
     )},
     (req, res, next) => {
-      T.get('friends/list', { screen_name: 'Ghir_Dario', count: 5 }, function(err, data, response) {
-        req.friends = data.users;
-        next();
-    }
-    )},
-    (req, res, next) => {
       T.get('direct_messages/sent', { count: 5 }, function(err, data, response) {
         req.messages = data;
+        next();
+    }
+  )},
+    (req, res, next) => {
+      T.get('friends/list', { screen_name: 'Ghir_Dario', count: 5 }, function(err, data, response) {
+        req.friends = data.users;
         next();
     }
     )}
 )
 
-app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   const { account, tweets, friends, messages } = req;
   res.render('index', { account, tweets, friends, messages });
 })
 
+
+app.post('/', (req, res) => {
+  T.post('statuses/update', { status: req.body.tweet }, function(err, data, response) {
+    res.redirect('/')
+  })
+})
+
+
+app.use((req, res, next) => {
+    const err = new Error('The page you are looking for is not available, sorry.');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
